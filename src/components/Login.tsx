df9ceef3d021a07671466cb348e4da4096ff71a9
AestheticMunda <aryamanagrawal099@gmail.com>
2024-08-05 14:23:01 +0530

    fixed buttons and otp backspace logic

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,7 +7,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+<<<<<<< Updated upstream
 import CreateProfile from "./CreateUser";
+=======
+import { FormType } from "@/types";
+>>>>>>> Stashed changes
 
 import {
 	Form,
@@ -36,6 +40,7 @@ const Login: React.FC<LoginProps> = ({
 	setFormType,
 	setFormData,
 }) => {
+<<<<<<< Updated upstream
 	const [emailOrPhoneNumber, setEmailOrPhoneNumber] = useState("");
 	const [password, setPassword] = useState("");
 	const [isClient, setIsClient] = useState(false);
@@ -48,6 +53,16 @@ const Login: React.FC<LoginProps> = ({
 	const [isSendingOTP, setIsSendingOTP] = useState(false);
 	const [token, setToken, deleteToken] = useCookie("token");
 	const requestOtp = useOtpRequest();
+=======
+  const [emailOrPhoneNumber, setEmailOrPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+  const [showPasswordInput, setShowPasswordInput] = useState(false);
+  const [isSendingOTP, setIsSendingOTP] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+>>>>>>> Stashed changes
 
 	// const [isLoading, setIsLoading] = useState(false);
 	const inputRef = useRef<HTMLInputElement>(null);
@@ -56,6 +71,7 @@ const Login: React.FC<LoginProps> = ({
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	const phoneRegex = /^\d{10}$/;
 
+<<<<<<< Updated upstream
 	const validateInput = (emailOrPhoneNumber: string): boolean => {
 		return (
 			emailRegex.test(emailOrPhoneNumber) ||
@@ -86,6 +102,47 @@ const Login: React.FC<LoginProps> = ({
 
 		try {
 			const { data } = await axiosInstance.post("/users/exists", userObj);
+=======
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!validateInput(emailOrPhoneNumber.trim())) {
+      setErrorMessage("Invalid email or mobile number.");
+      return;
+    }
+    setErrorMessage("");
+    setIsSendingOTP(true);
+    setFormData({
+      email: emailRegex.test(emailOrPhoneNumber) ? emailOrPhoneNumber : "",
+      phone: emailRegex.test(emailOrPhoneNumber) ? "" : emailOrPhoneNumber,
+    });
+    
+    const userObj = emailRegex.test(emailOrPhoneNumber)
+    ? { email: emailOrPhoneNumber, phone: "" }
+    : { email: "", phone: emailOrPhoneNumber };
+    
+    try {
+      const { data } = await axiosInstance.post("/users/exists", userObj);
+      
+      if (data.exists) {
+        setHasAccount(true);
+        setShowPasswordInput(true);
+        setIsSendingOTP(false);
+        
+        if (inputRef.current) {
+          inputRef.current.blur();
+        }
+      } else {
+        setHasAccount(false);
+        setFormType("otp");
+      }
+    } catch (error) {
+      console.error("Error checking user existence:", error);
+      setErrorMessage("Error checking user existence.");
+    } finally {
+      setIsSendingOTP(false);
+    }
+  };
+>>>>>>> Stashed changes
 
 			if (data.exists) {
 				setHasAccount(true);
@@ -114,6 +171,7 @@ const Login: React.FC<LoginProps> = ({
 		}
 	};
 
+<<<<<<< Updated upstream
 	const handlePasswordSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
 		if (password.trim() === "") {
@@ -274,6 +332,110 @@ const Login: React.FC<LoginProps> = ({
 			</div>
 		</div>
 	);
+=======
+    try {
+      const { data } = await axiosInstance.post("/users/login", {
+        ...(emailRegex.test(emailOrPhoneNumber)
+          ? { email: emailOrPhoneNumber }
+          : { phone: emailOrPhoneNumber }),
+        password,
+      });
+
+      console.log("Login successful:", data);
+
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, 5000);
+    } catch (error) {
+      console.error("Error logging in:", error);
+      setErrorMessage("Error logging in. Please try again.");
+    }
+  };
+
+  const form = useForm<AuthSchemaType>({
+    resolver: zodResolver(AuthSchema),
+  });
+
+  return (
+    <div>
+      <div className="flex flex-col sm:w-[320px] md:w-[380px] lg:w-[466px] bg-white p-8 rounded z-10">
+        <Form {...form}>
+          <form
+            onSubmit={showPasswordInput ? handlePasswordSubmit : handleSubmit}
+          >
+            <div>
+              <h4 className="text-2xl font-bold mb-5">Get Start New</h4>
+              <FormField
+                control={form.control}
+                name="email_or_phone_number"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      htmlFor="email_or_phone_number"
+                      className="pl-1 text-gray-500 text-sm"
+                    >
+                      Email or Phone Number
+                    </FormLabel>
+                    <FormControl>
+                      <div className="relative space-y-2">
+                        <Input
+                          id="email_or_phone_number"
+                          type="text"
+                          className="tracking-wider"
+                          placeholder="Enter Email or Phone Number"
+                          {...field}
+                          value={emailOrPhoneNumber}
+                          onChange={(e) =>
+                            setEmailOrPhoneNumber(e.target.value)
+                          }
+                          ref={inputRef}
+                        />
+                        {showPasswordInput && (
+                          <>
+                            <FormLabel
+                              htmlFor="password"
+                              className="pl-1 text-gray-500 text-sm"
+                            >
+                              Password
+                            </FormLabel>
+                            <Input
+                              id="password"
+                              type="password"
+                              className="border rounded sm:w-[250px] md:w-[320px] lg:w-[402px] border-gray-300 py-4 text-xs tracking-wider focus:to-blue-500 focus:border-blue-500 placeholder:text-gray-400"
+                              placeholder="Password"
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                          </>
+                        )}
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                variant={"lmsActive"}
+                className={`sm:w-[250px] md:w-[320px] lg:w-[402px] py-2 border-2 mt-5 font-semibold rounded mb-8`}
+              >
+                {isSendingOTP
+                ? !showPasswordInput
+                    ? "Please Wait..."
+                    : "Sending OTP..."
+                  : showPasswordInput
+                  ? "Login"
+                  : "Submit"}
+              </Button>
+              {errorMessage && (
+                <div className="text-red-500 mt-2">{errorMessage}</div>
+              )}
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default Login;
