114ee96ab4324f977885eaca10bf70d590edfeb1
Akshay Jain <akshayjain2823@gmail.com>
2024-08-26 12:33:52 +0530

    Class, add teacher, add student

--- src/app/(root)/viewclass/columns.tsx
+++ src/app/(root)/viewclass/columns.tsx
@@ -2,38 +2,93 @@
 
 import ActionCell from "@/components/ActionCell";
 import ClassActionCell from "@/components/ClassActionCell";
+import { Button } from "@/components/ui/button";
 import { setViewState } from "@/context/staffSlice";
 import { fetchStudentById } from "@/context/studentSlice";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type EnrolledStudent = {
-  id: string;
-  studentName: string;
-  fatherName: string;
-  class: string;
-  rollNumber: string;
+	id: string;
+	name: string;
+	fatherName: string;
+	class: string;
+	rollNumber: string;
 };
 
 export const columns: ColumnDef<EnrolledStudent>[] = [
-  {
-    accessorKey: "studentName",
-    header: "Student Name",
-  },
-  {
-    accessorKey: "parentName",
-    header: "Parent Name",
-  },
-  {
-    accessorKey: "class",
-    header: "Class",
-  },
-  {
-    accessorKey: "rollNumber",
-    header: "Roll Number",
-  },
-  {
+	{
+		accessorKey: "rollNumber",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					className='p-0 outline-none border-none uppercase font-bold text-lmsBase leading-4 tracking-wider text-lmsPrimary hover:bg-white'
+					onClick={() =>
+						column.toggleSorting(column.getIsSorted() === "asc")
+					}
+				>
+					Roll Number
+					<ArrowUpDown className='ml-2 h-4 w-4' />
+				</Button>
+			);
+		},
+	},
+	{
+		accessorKey: "name",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					className='p-0 outline-none border-none uppercase font-bold text-lmsBase leading-4 tracking-wider text-lmsPrimary hover:bg-white'
+					onClick={() =>
+						column.toggleSorting(column.getIsSorted() === "asc")
+					}
+				>
+					Student Name
+					<ArrowUpDown className='ml-2 h-4 w-4' />
+				</Button>
+			);
+		},
+	},
+	{
+		accessorKey: "parentName",
+		header: ({ column }) => {
+			return (
+				<Button
+					variant='ghost'
+					className='p-0 outline-none border-none uppercase font-bold text-lmsBase leading-4 tracking-wider text-lmsPrimary hover:bg-white'
+					onClick={() =>
+						column.toggleSorting(column.getIsSorted() === "asc")
+					}
+				>
+					Parent Name
+					<ArrowUpDown className='ml-2 h-4 w-4' />
+				</Button>
+			);
+		},
+	},
+	// {
+	// 	accessorKey: "class",
+	// 	header: ({ column }) => {
+	// 		return (
+	// 			<Button
+	// 				variant='ghost'
+	// 				className='p-0 outline-none border-none uppercase font-bold text-lmsBase leading-4 tracking-wider text-lmsPrimary hover:bg-white'
+	// 				onClick={() =>
+	// 					column.toggleSorting(column.getIsSorted() === "asc")
+	// 				}
+	// 			>
+	// 				Class
+	// 				<ArrowUpDown className='ml-2 h-4 w-4' />
+	// 			</Button>
+	// 		);
+	// 	},
+	// },
+
+	{
 		id: "actions",
 		enableHiding: false,
 		cell: ({ row }) => (
