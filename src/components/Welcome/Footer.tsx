5168bc3878eae9849c8fe574752a912fb285dd18
Akshay Jain <akshayjain2823@gmail.com>
2024-10-21 16:08:48 +0530

    footer fix

--- src/components/Welcome/Footer.tsx
+++ src/components/Welcome/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
 				<div className='text-center'>
 					<h3 className='text-3xl mb-3'> Download our mobile app </h3>
 					<p> Stay connected. All day, every day. </p>
-					<div className='flex justify-center my-10'>
+					<div className='flex justify-center my-5'>
 						<div className='flex items-center border rounded-lg px-4 py-2 w-52 mx-2'>
 							<img
 								src='https://cdn-icons-png.flaticon.com/512/888/888857.png'
@@ -33,10 +33,10 @@ const Footer = () => {
                 </div> */}
 					</div>
 				</div>
-				<div className='mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400'>
+				<div className='mt-14 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400'>
 					<p className='order-2 md:order-1 mt-8 md:mt-0'>
 						{" "}
-						&copy; LMS Buddy, 2021.{" "}
+						&copy; LMS Buddy, 2024.{" "}
 					</p>
 					<div className='order-1 md:order-2'>
 						{/* <Link href={'/'}>
